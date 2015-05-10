f122b8ce5625ee8ab9085c2476530adc2fde24b6
dani <dani-kenny@163.com>
2015-05-11 00:13:38 +0800

    修改

--- Application/Admin/Controller/TestController.class.php
+++ Application/Admin/Controller/TestController.class.php
@@ -1,16 +1,6 @@
 <?php
 use Think\Controller;
 class TestController extends CommonController{
-// 	public function index()
-// 	{
-		
-// 		$model=D('User');
-//     	$map='';
-//     	$voList = $model->where($map)->select();
-//     	dump($voList);
-//     	$this->assign('list',$voList);
-//     	 $this->display();
-		
-// 	}
+
 } 
 ?>
